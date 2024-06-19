4b0c3837a25805ee412f4205e5d7e679727b4c1b
z_moff <zmoffzema@gmail.com>
2024-06-20 00:06:01 +0300

    Добавьте файлы проекта.

---
+++ 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Заданный массив целых чисел
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.WriteLine("Нечетные числа из массива:");
+
+        // Перебираем элементы массива и выводим только нечетные числа
+        foreach (int number in numbers)
+        {
+            if (number % 2 != 0)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
